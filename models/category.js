701f72a61ea0f1132ee77ae77214a7220113d6ae
ifarbie <farizrifkyberliano@gmail.com>
2024-06-19 19:10:13 +0700

    feat: add endpoint for categories

---
+++ models/category.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Category.init(
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING(20),
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Category",
+    }
+  );
+  return Category;
+};
