dcce4614e1112d61a6ffac7f277148857f3bf3a9
ifarbie <farizrifkyberliano@gmail.com>
2024-06-23 19:51:16 +0700

    feat: migrate table bridgeproductcategory, add category by id route, define association in product and category model

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,10 +1,11 @@
-const {products} = require("../models");
+const {products, Category} = require("../models");
 
 const getProductDetail = async (req, res, next) => {
     const data = await products.findOne({
       where: {
         id: req.params.id,
-      }
+      },
+      include: [Category]
     });
     return res.status(200).json(data);
   };
