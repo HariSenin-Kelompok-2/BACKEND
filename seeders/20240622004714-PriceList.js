acf18f50b7e5fcf7682fa6d609433a73f003159b
DarDarDinks22 <indracrow0@gmail.com>
2024-06-25 01:59:55 +0700

    price list product relation

--- seeders/20240622004714-PriceList.js
+++ seeders/20240622004714-PriceList.js
@@ -19,7 +19,7 @@ module.exports = {
           id: "0030d395-1e25-4c76-a481-1e678876c331",
           price: "Rp.8,999",
           discount: 10,
-          productId: "fa02177d-bd6a-4de5-a2ac-08a50942d805",
+          productId: "8459da6e-89a1-4931-a05a-2303ad43c532",
         },
         {
           id: "f122d34b-bc15-4e54-a243-aa78aa43fa58",
