f93fdb85794fe76a9928e425e1bff4206c3ff228
fajri-farid <fajrijry14@gmail.com>
2024-06-24 22:29:12 +0800

    feel id manual

--- seeders/20240624130255-Carts.js
+++ seeders/20240624130255-Carts.js
@@ -16,12 +16,12 @@ module.exports = {
       "Carts",
       [
         {
-          id: crypto.randomUUID(),
+          id: "9130dde5-da6f-4ba7-916c-ae161fe7ee01",
           priceListId: "0030d395-1e25-4c76-a481-1e678876c331",
           UserId: "5194f115-92c4-405c-905e-a37aaee94ad4",
         },
         {
-          id: crypto.randomUUID(),
+          id: "d2a156a7-ab32-4bd8-8f52-6bb71c23d6c7",
           priceListId: "f122d34b-bc15-4e54-a243-aa78aa43fa58",
           UserId: "5194f115-92c4-405c-905e-a37aaee94ad4",
         },
