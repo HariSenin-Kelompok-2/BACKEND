653f09b4444a4c2d733930a67964607f7dbe249e
ifarbie <farizrifkyberliano@gmail.com>
2024-06-29 19:01:58 +0700

    feat: add error handler in user controller and change put to patch for user update

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,49 +2,57 @@ const { Users, Region, products, Carts, PriceList, Review } = require("../models
 const { signToken, hashPassword, checkUsername } = require("../services");
 
 const getUserDetail = async (req, res, next) => {
-  const data = await Users.findOne({
-    where: {
-      id: req.currentUser.id,
-    },
-    attributes: ["username", "email"],
-    include: [
-      {
-        model: Region,
-        attributes: ["name"],
+  try {
+    const data = await Users.findOne({
+      where: {
+        id: req.currentUser.id,
       },
-      {
-        model: products,
-        as: "productOwned",
-        attributes: ["name"],
-      },
-      {
-        model: Carts,
-        attributes: ["id"],
-        include: [
-          {
-            model: PriceList,
-            attributes: ["price", "discount"],
-            include: [
-              {
-                model: products,
-                attributes: ["name"],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        model: Review,
-        attributes: ["id", "content", "isRecommend"],
-        include: {
-          model: products,
+      attributes: ["username", "email"],
+      include: [
+        {
+          model: Region,
           attributes: ["name"],
-        }
-      }
-    ],
-  });
+        },
+        {
+          model: products,
+          as: "productOwned",
+          attributes: ["name"],
+        },
+        {
+          model: Carts,
+          attributes: ["id"],
+          include: [
+            {
+              model: PriceList,
+              attributes: ["price", "discount"],
+              include: [
+                {
+                  model: products,
+                  attributes: ["name"],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          model: Review,
+          attributes: ["id", "content", "isRecommend"],
+          include: {
+            model: products,
+            attributes: ["name"],
+          },
+        },
+      ],
+    });
 
-  return res.status(200).json(data);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
 
 const registerUser = async (req, res, next) => {
@@ -124,131 +132,155 @@ const registerUser = async (req, res, next) => {
 
     return res.status(201).json({ status: 201, message: "berhasil register!", data });
   } catch (error) {
-    res.status(400).json({
-      message: "Validasi Error",
-      error: error.errors.map((err) => err.message),
+    res.status(500).json({
+      message: "Internal server Error",
+      error: error.message,
       // error,
     });
   }
 };
 
 const loginUser = async (req, res, next) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  if (!username || !password) {
-    return res.status(400).json({ message: "username dan password harus diisi!" });
-  }
-
-  const exist = await Users.findOne({
-    where: {
-      username,
-    },
-  });
-
-  if (!exist || !(await exist.isCorrectPassword(password, exist.password))) {
-    return res.status(400).json({ message: "invalid username or password" });
-  }
-
-  const token = signToken(exist.id);
-  res.setHeader("Authorization", `Bearer ${token}`);
-
-  return res.status(200).json({ message: "berhasil login!" });
-};
-
-const updateUsers = async (req, res, next) => {
-  const { username, email, password, passwordConfirm, region } = req.body;
-  const data = await Users.findOne({
-    where: {
-      id: req.currentUser.id,
-    },
-  });
-
-  if (!data) {
-    return res.status(200).json({
-      messsage: "data tidak ada!",
-    });
-  }
-
-  if (region) {
-    const existRegion = await Region.findOne({
-      where: {
-        name: region,
-      },
-    });
-
-    if (!existRegion) {
-      return res.status(404).json({ message: "region tidak ditemukan" });
+    if (!username || !password) {
+      return res.status(400).json({ message: "username dan password harus diisi!" });
     }
 
-    data.regionId = existRegion.id;
-  }
-
-  if (email) {
-    const exists = await Users.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (exists && exists.id !== data.id) {
-      return res.status(400).json({
-        message: "Email sudah digunakan",
-      });
-    }
-
-    data.email = email;
-  }
-
-  if (username) {
-    const existUsername = await Users.findOne({
+    const exist = await Users.findOne({
       where: {
         username,
       },
     });
 
-    if (existUsername && existUsername.id !== data.id) {
-      return res.status(400).json({
-        message: "Username sudah digunakan",
+    if (!exist || !(await exist.isCorrectPassword(password, exist.password))) {
+      return res.status(400).json({ message: "invalid username or password" });
+    }
+
+    const token = signToken(exist.id);
+    res.setHeader("Authorization", `Bearer ${token}`);
+
+    return res.status(200).json({ message: "berhasil login!" });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+const updateUsers = async (req, res, next) => {
+  try {
+    const { username, email, password, passwordConfirm, region } = req.body;
+    const data = await Users.findOne({
+      where: {
+        id: req.currentUser.id,
+      },
+    });
+
+    if (!data) {
+      return res.status(200).json({
+        messsage: "data tidak ada!",
       });
     }
 
-    data.username = username;
-  }
+    if (region) {
+      const existRegion = await Region.findOne({
+        where: {
+          name: region,
+        },
+      });
 
-  if (password) {
-    if (!passwordConfirm) {
-      return res.status(400).json({ message: "jika ubah password, password dan passwordConfirm harus diisi!" });
+      if (!existRegion) {
+        return res.status(404).json({ message: "region tidak ditemukan" });
+      }
+
+      data.regionId = existRegion.id;
     }
 
-    if (password !== passwordConfirm) {
-      return res.status(400).json({ message: "password dan passwordConfirm tidak cocok" });
+    if (email) {
+      const exists = await Users.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (exists && exists.id !== data.id) {
+        return res.status(400).json({
+          message: "Email sudah digunakan",
+        });
+      }
+
+      data.email = email;
     }
 
-    data.password = await hashPassword(password);
-  }
+    if (username) {
+      const existUsername = await Users.findOne({
+        where: {
+          username,
+        },
+      });
 
-  await data.save();
-  return res.status(201).json({ status: "berhasil update user!", data });
+      if (existUsername && existUsername.id !== data.id) {
+        return res.status(400).json({
+          message: "Username sudah digunakan",
+        });
+      }
+
+      data.username = username;
+    }
+
+    if (password) {
+      if (!passwordConfirm) {
+        return res.status(400).json({ message: "jika ubah password, password dan passwordConfirm harus diisi!" });
+      }
+
+      if (password !== passwordConfirm) {
+        return res.status(400).json({ message: "password dan passwordConfirm tidak cocok" });
+      }
+
+      data.password = await hashPassword(password);
+    }
+
+    await data.save();
+    return res.status(201).json({ status: "berhasil update user!", data });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 };
 
 const deleteUsers = async (req, res, next) => {
-  const data = await Users.findOne({
-    where: {
-      id: req.currentUser.id,
-    },
-  });
+  try {
+    const data = await Users.findOne({
+      where: {
+        id: req.currentUser.id,
+      },
+    });
 
-  if (!data) {
-    return res.status(404).json({
-      messsage: "data tidak ada",
+    if (!data) {
+      return res.status(404).json({
+        messsage: "data tidak ada",
+      });
+    }
+
+    await data.destroy();
+    return res.status(200).json({
+      message: "berhasil delete user!",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      code: 500,
+      message: "Internal Server Error",
+      error: error.message,
     });
   }
-
-  await data.destroy();
-  return res.status(200).json({
-    message: "berhasil delete user!",
-    data,
-  });
 };
 
 module.exports = {
