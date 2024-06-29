64afcae62168d2144854613bebd1328a3b367ed7
ifarbie <farizrifkyberliano@gmail.com>
2024-06-29 23:12:15 +0700

    changes: changed output on category controller and pricelist seeders

--- controllers/category.controllers.js
+++ controllers/category.controllers.js
@@ -1,18 +1,37 @@
-const { Category, products } = require("../models");
+const { Category, products, PriceList, Feature } = require("../models");
 
 const getCategories = async (req, res) => {
-  const categories = await Category.findAll({ include: [products] });
+  const categories = await Category.findAll({
+    attributes: ["id", "name"],
+  });
   return res.status(200).json({ code: 200, data: categories });
 };
 
 const getCategoryById = async (req, res) => {
   const { id } = req.params;
   const category = await Category.findOne({
+    attributes: ["id", "name"],
     where: {
-      id
+      id,
     },
-    include: [products]
-  })
+    include: [
+      {
+        model: products,
+        attributes: ["id", "name", "short_description", "product_thumbnail"],
+        include: [
+          {
+            model: PriceList,
+            attributes: ["price", "discount"]
+          },
+          {
+            model: Feature,
+            as: "productFeatures",
+            attributes: ["name", "icon"]
+          }
+        ]
+      },
+    ],
+  });
   return res.status(200).json({ code: 200, data: category });
 };
 
--- seeders/20240622004714-PriceList.js
+++ seeders/20240622004714-PriceList.js
@@ -17,13 +17,13 @@ module.exports = {
       [
         {
           id: "0030d395-1e25-4c76-a481-1e678876c331",
-          price: "Rp.8,999",
+          price: "549000",
           discount: 10,
           productId: "8459da6e-89a1-4931-a05a-2303ad43c532",
         },
         {
           id: "f122d34b-bc15-4e54-a243-aa78aa43fa58",
-          price: "Rp.8,9992",
+          price: "0",
           discount: 0,
           productId: "fa02177d-bd6a-4de5-a2ac-08a50942d805",
         },
