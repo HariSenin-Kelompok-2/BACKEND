40975657f85c19cebc395e9a70f7818846c5ca25
ifarbie <farizrifkyberliano@gmail.com>
2024-07-01 07:58:20 +0700

    refactor: services and split queries to services

---
+++ services/categoryQuery.services.js
@@ -0,0 +1,36 @@
+const { Category, products, PriceList, Feature } = require("../models");
+
+const getCategoryWhere = async (columnObject) => {
+  const category = await Category.findOne({
+    attributes: ["id", "name"],
+    where: {
+      ...columnObject,
+    },
+    include: [
+      {
+        model: products,
+        attributes: ["id", "name", "short_description", "product_thumbnail"],
+        include: [
+          {
+            model: PriceList,
+            attributes: ["price", "discount"],
+          },
+          {
+            model: Feature,
+            as: "productFeatures",
+            attributes: ["name", "icon"],
+          },
+        ],
+      },
+    ],
+  });
+  return category;
+};
+
+const getAllCategories = async () => {
+  return await Category.findAll({
+    attributes: ["id", "name"],
+  });
+};
+
+module.exports = { getCategoryWhere, getAllCategories };
--- services/checkUsername.js
+++
@@ -1,6 +0,0 @@
-const checkUsername = (username) => {
-  const regex = /^[a-zA-Z0-9]+$/;
-  return regex.test(username);
-};
-
-module.exports = checkUsername;
