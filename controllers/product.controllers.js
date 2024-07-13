17eb8ec6412676f1bc0ecf9a869a942f2456a770
ifarbie <farizrifkyberliano@gmail.com>
2024-07-13 11:24:25 +0700

    rapihin product controllers

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,107 +1,77 @@
-const {products, Category, PriceList, Feature, SysReqs, CategorySysReq, Review, ScrollThumbnail, descImg} = require("../models");
+const { products, Category, PriceList, Feature, SysReqs, CategorySysReq, Review, ScrollThumbnail, descImg } = require("../models");
 
 const getProductDetail = async (req, res, next) => {
-  try{
+  try {
     const productData = req.params.id;
     const data = await products.findOne({
-      attributes: [
-        "id",
-        "name",
-        "short_description",
-        "release_date",
-        "developer",
-        "publisher",
-        "product_thumbnail",
-        "video",
-      ],
+      attributes: ["id", "name", "short_description", "release_date", "developer", "publisher", "product_thumbnail", "video"],
       where: {
         id: req.params.id,
-     },
+      },
       include: [
         {
           model: Category,
-          attributes: [
-            "id",
-            "name",
-          ]
-        }, 
-        {
-          model: PriceList,
-          attributes: [
-            "id",
-            "price",
-            "discount"
-          ]      
-        }, 
-        {
-          model: Feature, 
-          as: "productFeatures",
-          attributes: [
-            "id",
-            "name",
-            "icon",
-          ]
-        }, 
-        {
-          model: SysReqs,
-          attributes: [
-            "id",
-            "productId",
-            "recommended",
-            "osId",
-            "processor",
-            "memory",
-            "graphics",
-            "directW",
-            "storage",
-          ],
-          include: [
-            {
-              model: CategorySysReq
-            }
-          ]
+          attributes: ["id", "name"],
+          through: {
+            attributes: []
+          }
         },
         {
-          model: Review
+          model: PriceList,
+          attributes: ["id", "price", "discount"],
+        },
+        {
+          model: Feature,
+          as: "productFeatures",
+          attributes: ["id", "name", "icon"],
+        },
+        {
+          model: SysReqs,
+          attributes: ["id", "productId", "recommended", "osId", "processor", "memory", "graphics", "directX", "storage"],
+          include: [
+            {
+              model: CategorySysReq,
+            },
+          ],
+        },
+        {
+          model: Review,
         },
         {
           model: ScrollThumbnail,
-          attributes: [
-            "id",
-            "productId",
-            "img"
-          ]
+          attributes: ["id", "productId", "img"],
         },
         {
           model: descImg,
-        }
-    ]
-  });
-  if (!data){
-    throw new Error("Data tidak ditemukan")
-  }  
-  if(productData){
-  return res.status(200).json({ 
-                                code: 200, 
-                                message: "success", 
-                                data: data })};                          
+        },
+      ],
+    });
+    if (!data) {
+      throw new Error("Data tidak ditemukan");
+    }
+    if (productData) {
+      return res.status(200).json({
+        code: 200,
+        message: "success",
+        data: data,
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({
+      code: 404,
+      message: "Page Not Found",
+      error
+    });
   }
-    catch (error){
-  return res.status(404).json({
-                                code:404,
-                                message: "Page Not Found",
-  })
-}}
+};
 
-    
+const getAllProduct = async (req, res, next) => {
+  const data = await products.findAll({ include: [PriceList] });
+  return res.status(200).json({
+    code: 200,
+    message: "success",
+    data: data,
+  });
+};
 
-  const getAllProduct = async (req, res, next) => {
-    const data = await products.findAll({include: [PriceList]});
-    return res.status(200).json({ 
-                                  code: 200, 
-                                  message: "success", 
-                                  data: data });
-  };
-
-  module.exports = {getProductDetail, getAllProduct};
-  
+module.exports = { getProductDetail, getAllProduct };
